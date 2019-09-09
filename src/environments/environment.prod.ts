4f0cab446a5a2f3fb3d88c1f51b001324007dca8
Stefan <marjanovic.steff@gmail.com>
2019-09-09 19:13:12 +0200

    renewing api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  agmApi: 'AIzaSyDuS3MZKX7uRyRPThcuPMDO4Kp5j77fpHU',
+  foursquare: {
+    clientID: 'CF00V4VUDHU1BQBUEOBJ1GMEKLOWTQKTGNNBS5AMKUIOLROY',
+    clientSecret: '0KGHKBEEDJWDUXJOJTBNRUR0ABTD00EC4ASDFSDP3TVWBR2G'
+  },
+  edamam: {
+    appID: 'daa673ca',
+    appKey: '608b42477eef1e0eea7e7d6087e8c6ea'
+  }
 };
